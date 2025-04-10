828fd5415046849ffa8105f6070e337d01a8834d
Dex <a.markdexter@gmail.com>
2025-04-11 00:01:34 +0800

    autoload

--- src/app/PaymentGateway/Paddle/Transaction.php
+++ src/app/PaymentGateway/Paddle/Transaction.php
@@ -2,7 +2,7 @@
 
 declare(strict_types = 1);
 
-namespace PaymentGateway\Paddle;
+namespace App\PaymentGateway\Paddle;
 
 // use Notification\Email;
 
--- src/public/index.php
+++ src/public/index.php
@@ -1,17 +1,31 @@
 <?php
 
-require_once '../PaymentGateway/Paddle/Transaction.php';
-require_once '../PaymentGateway/Stripe/Transaction.php';
-require_once '../PaymentGateway/Paddle/CustomerProfile.php';
-require_once '../Notification/Email.php';
+// require_once '../PaymentGateway/Paddle/Transaction.php';
+// require_once '../PaymentGateway/Stripe/Transaction.php';
+// require_once '../PaymentGateway/Paddle/CustomerProfile.php';
+// require_once '../Notification/Email.php';
 
-use PaymentGateway\Paddle\{Transaction, CustomerProfile};
-use PaymentGateway\Stripe\Transaction as StripeTransaction;
+// use PaymentGateway\Paddle\{Transaction, CustomerProfile};
+// use PaymentGateway\Stripe\Transaction as StripeTransaction;
+
+// $paddleTransaction = new Transaction();
+// $stripeTransaction = new StripeTransaction();
+// $paddleCustomerProfile = new CustomerProfile();
+
+// var_dump($paddleTransaction, $stripeTransaction, $paddleCustomerProfile);
+
+// include 'views/layout'; // the use aboove are not applied here need to add it manually
+
+require __DIR__ . '/../vendor/autoload.php';
+
+use App\PaymentGateway\Paddle\Transaction;
+use Ramsey\Uuid\UuidFactory;
 
 $paddleTransaction = new Transaction();
-$stripeTransaction = new StripeTransaction();
-$paddleCustomerProfile = new CustomerProfile();
 
-var_dump($paddleTransaction, $stripeTransaction, $paddleCustomerProfile);
+$factory = new UuidFactory();
+$uuid = $factory->uuid4();
 
-include 'views/layout'; // the use aboove are not applied here need to add it manually
+echo $uuid->toString();
+
+var_dump($paddleTransaction);
