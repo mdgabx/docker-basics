10dba842c2af456c0bd7fd9255d1ddb7da434534
Dex <a.markdexter@gmail.com>
2025-04-11 23:30:36 +0800

    constants

--- src/app/PaymentGateway/Paddle/Transaction.php
+++ src/app/PaymentGateway/Paddle/Transaction.php
@@ -4,13 +4,29 @@ declare(strict_types = 1);
 
 namespace App\PaymentGateway\Paddle;
 
+use App\Enums\Status;
 // use Notification\Email;
 
 
 class Transaction 
 {
+
+	private string $status;
+
 	public function __construct() 
 	{
-		
+		$this->setStatus(Status::DECLINED);
+	}
+
+	public function setStatus(string $status): self 
+	{
+		if(! isset(Status::ALL_STATUSES[$status])) 
+		{
+			throw new \InvalidArgumentException('invalid status');
+		}
+
+		$this->status = $status;
+
+		return $this;
 	}
 }
--- src/public/index.php
+++ src/public/index.php
@@ -1,31 +1,13 @@
 <?php
 
-// require_once '../PaymentGateway/Paddle/Transaction.php';
-// require_once '../PaymentGateway/Stripe/Transaction.php';
-// require_once '../PaymentGateway/Paddle/CustomerProfile.php';
-// require_once '../Notification/Email.php';
 
-// use PaymentGateway\Paddle\{Transaction, CustomerProfile};
-// use PaymentGateway\Stripe\Transaction as StripeTransaction;
+require_once __DIR__ . '/../vendor/autoload.php';
 
-// $paddleTransaction = new Transaction();
-// $stripeTransaction = new StripeTransaction();
-// $paddleCustomerProfile = new CustomerProfile();
-
-// var_dump($paddleTransaction, $stripeTransaction, $paddleCustomerProfile);
-
-// include 'views/layout'; // the use aboove are not applied here need to add it manually
-
-require __DIR__ . '/../vendor/autoload.php';
-
+use App\Enums\Status;
 use App\PaymentGateway\Paddle\Transaction;
-use Ramsey\Uuid\UuidFactory;
 
-$paddleTransaction = new Transaction();
+$transaction = new Transaction();
 
-$factory = new UuidFactory();
-$uuid = $factory->uuid4();
+$transaction->setStatus(Status::PAID);
 
-echo $uuid->toString();
-
-var_dump($paddleTransaction);
+var_dump($transaction);
