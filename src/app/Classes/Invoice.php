f34bd62ffc4f348de372ee530fa145ffc389138a
Dex <a.markdexter@gmail.com>
2025-04-19 17:09:26 +0800

    adding class

---
+++ src/app/Classes/Invoice.php
@@ -0,0 +1,18 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Classes;
+
+class Invoice 
+{
+    public function index(): string
+    {
+        return 'invoice';
+    }
+
+    public function create(): string
+    {
+        return 'Cretae';
+    }
+}
--- src/public/index.php
+++ src/public/index.php
@@ -1,18 +1,12 @@
 <?php
 
-use App\Invoice;
-
-
 require_once __DIR__ . '/../vendor/autoload.php';
 
 $router = new App\Router();
 
-$router->register('/', function () {
-    echo 'home';
-});
-
-$router->register('/invoices', function () {
-    echo 'invoices';
-});
+$router
+    ->register('/', [App\Classes\Home::class, 'index'])
+    ->register('/invoices', [App\Classes\Invoice::class, 'index'])
+    ->register('/invoices/create', [App\Classes\Invoice::class, 'create']);
 
 echo $router->resolve($_SERVER['REQUEST_URI']);
