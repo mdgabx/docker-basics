5039033ffff6d47259f1d21a1ed400fb3621d329
Dex <a.markdexter@gmail.com>
2025-04-20 00:46:24 +0800

    superglobal get and post

--- src/app/Classes/Invoice.php
+++ src/app/Classes/Invoice.php
@@ -13,6 +13,21 @@ class Invoice
 
     public function create(): string
     {
-        return 'Cretae';
+        return '<form
+            action="/invoices/create"
+            method="post"
+            >
+             <label>
+                Amount
+             </label>
+            <input type="text" id="amount" name="amount" />
+        </form>';
+    }
+
+    public function store()
+    {
+        $amount = $_POST['amount'];
+
+        var_dump($amount);
     }
 }
