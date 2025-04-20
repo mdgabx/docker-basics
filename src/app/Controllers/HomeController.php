0dcfcc68a4ff2de8d4c8c7655dae8bca8c17b538
Dex <a.markdexter@gmail.com>
2025-04-20 18:41:47 +0800

    headers and response

--- src/app/Controllers/HomeController.php
+++ src/app/Controllers/HomeController.php
@@ -13,13 +13,26 @@ class HomeController
         return View::make('index', ['foo' => 'bar']);
     }
 
+    public function download() 
+    {
+        header('Content-Type: application/png');
+        header('Content-Disposition: attachment;filename="myfile.png"');
+
+        readfile(STORAGE_PATH . '/chrome_cjLzcbTBdm.png');
+    }
+
     public function upload()
     { 
         $filePath = STORAGE_PATH . '/' . $_FILES['receipt']['name'];
         move_uploaded_file($_FILES['receipt']['tmp_name'], $filePath);
 
-        echo '<pre>';
-        var_dump(pathinfo($filePath));
-        echo '</pre>';
+        // echo '<pre>';
+        // var_dump(pathinfo($filePath));
+        // echo '</pre>';
+
+        header('Location: /');
+        exit;
+
+        // unlink(STORAGE_PATH . '/chrome_cjLzcbTBdm.png');
     }
 }
