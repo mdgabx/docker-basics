1159f2a1a2935b69cf9adc895c0ab4428f05b067
Dex <a.markdexter@gmail.com>
2025-04-20 13:46:26 +0800

    file uploads

--- src/app/Classes/Home.php
+++ src/app/Classes/Home.php
@@ -8,6 +8,22 @@ class Home
 {
     public function index(): string
     {
-        return 'home';
+        return <<<FORM
+            <form action="/upload" method="post" enctype="multipart/form-data">  
+                <input type="file" name="receipt[]" />
+                 <input type="file" name="receipt[]" />
+                <button type="submit">Upload</button>
+            </form>
+        FORM;
+    }
+
+    public function upload()
+    { 
+        $filePath = STORAGE_PATH . '/' . $_FILES['receipt']['name'];
+        move_uploaded_file($_FILES['receipt']['tmp_name'], $filePath);
+
+        echo '<pre>';
+        var_dump(pathinfo($filePath));
+        echo '</pre>';
     }
 }
--- src/public/index.php
+++ src/public/index.php
@@ -4,10 +4,13 @@ require_once __DIR__ . '/../vendor/autoload.php';
 
 session_start();
 
+define('STORAGE_PATH', __DIR__ . '/../storage');
+
 $router = new App\Router();
 
 $router
     ->get('/', [App\Classes\Home::class, 'index'])
+    ->post('/upload', [App\Classes\Home::class, 'upload'])
     ->get('/invoices', [App\Classes\Invoice::class, 'index'])
     ->get('/invoices/create', [App\Classes\Invoice::class, 'create'])
     ->post('/invoices/create', [App\Classes\Invoice::class, 'store']);
